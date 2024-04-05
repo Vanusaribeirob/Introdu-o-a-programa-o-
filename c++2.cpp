a3990578d21145d007ab0ad95204ef62202b8061
Vanusaribeirob <161831491+Vanusaribeirob@users.noreply.github.com>
2024-04-05 08:47:23 -0300

    Add files via upload

---
+++ c++2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	float valor1, valor2, media;
+	cin>>valor1;
+	cin>>valor2;
+	media=(valor1+valor2)/2;
+	cout<<media;
+	
+	return 0;
+}
+	
+	
